be554a721096a7a961429d7689d09bfd7e064ae4
Eduar Ursu <eduardursu78@gmail.com>
2021-06-17 18:21:13 +0200

    v0.0.1

--- lib_min.js
+++ lib_min.js
@@ -46,44 +46,30 @@ function arrayBufferToBase64(buffer) {
     bytes.forEach((b) => binary += String.fromCharCode(b));
     return window.btoa(binary);
 }
-function pagesRoute(){
-  var t=this;
-  const notFoundPage = document.querySelector("#notFound");
-  t.links = Array.from(document.querySelectorAll('[topage]'));
-  t.scrollTop = ()=>{
-    document.querySelector('html').scrollTop = 0;
-    document.querySelector('body').scrollTop = 0;
-  }
-  t.navigate = (id) =>{
-    //Hide current active page
-    var activePage = document.querySelector("section.page-active");
-    if (activePage) activePage.classList.remove("page-active");
-    var activeLink = document.querySelector('[topage].active');
-    if (activeLink) activeLink.classList.remove("active");
-    //Show the next page
-    var nextPage = document.querySelector(id);
-    if (nextPage) nextPage.classList.add("page-active");
-    var nextLink = document.querySelector("[topage='"+id+"']");
-    if(nextLink) nextLink.classList.add("active");
-    //Scroll to top
-    t.scrollTop();
-    //Set history state
-    if (history.pushState)
-      history.pushState(null, null, id);
-    else 
-      location.hash = id;
-  }
-  t.listeners= ()=>{
-    t.links.forEach((page)=>{
+function pagesRoute() {
+  const link = Array.from(document.querySelectorAll('[topage]'));
+  if (link) {
+    const navigate = (id) => {
+      var activePage = document.querySelector("section.page-active");
+      var activeLink = document.querySelector("[topage].active");
+      if (activePage)
+        activePage.classList.remove("page-active");
+      if (activeLink)
+        activeLink.classList.remove("active");
+      var nextPage = document.querySelector(id);
+      var nextLink = document.querySelector("[topage='" + id + "']");
+      if (nextPage)
+        nextPage.classList.add("page-active");
+      if (nextLink)
+        nextLink.classList.add("active");
+    }
+    link.forEach(function (page) {
       var id = page.getAttribute("topage");
-      page.addEventListener('click', ()=>{t.navigate(id)});
+      page.addEventListener('click', function () {
+        navigate(id)
+      });
     })
   }
-  if(t.links){
-    if (window.location.hash)
-        t.navigate(window.location.hash);
-    t.listeners();
-    }
 }
 //Function to preconnect to an url
 function preconnectTo(url) {
