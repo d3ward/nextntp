1b5671b03c1dc9f3bc72ba75eb46fd453452b19d
Oranko <Orankosen@gmail.com>
2021-08-11 13:18:15 +0530

    dark mode shadow

--- nextntp.js
+++ nextntp.js
@@ -2184,13 +2184,3 @@ function addShadow() {
 function removeShadow() {
   document.getElementById("sb_input").style.boxShadow = "none";
 }
-
-document.getElementById("tlg_item").addEventListener("focus", addShadow1);
-document.getElementById("tlg_item").addEventListener("blur", removeShadow2);
-
-function addShadow1() {
-  document.getElementById("tlg_item").style.boxShadow = "var(--o0)";
-}
-function removeShadow2() {
-  document.getElementById("tlg_item").style.boxShadow = "none";
-}
