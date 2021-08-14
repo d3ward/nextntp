02f7af452382fe343cfd97b8a0425d0d032587ce
Eduar Ursu <eduardursu78@gmail.com>
2021-08-14 21:28:23 +0200

    Update service worker

--- sw.js
+++ sw.js
@@ -23,14 +23,33 @@ self.addEventListener('activate', (event) => {
 });
 
 // When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
-self.addEventListener('fetch', (event) => {
-    console.log('Fetch intercepted for:', event.request.url);
-    event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            if (cachedResponse) {
-                return cachedResponse;
-            }
-            return fetch(event.request);
-        }),
-    );
+self.addEventListener('fetch', (e) => {
+    console.log('Fetch intercepted for:', e.request.url);
+    e.respondWith((async function() {
+        const cachedResponse = await caches.match(e.request);
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+    
+        const networkResponse = await fetch(e.request);
+    
+        const hosts = [
+          'https://lh3.googleusercontent.com/proxy',
+          'https://logos.kiwibrowser.com'
+        ];
+    
+        if (hosts.some((host) => e.request.url.startsWith(host))) {
+          // This clone() happens before `return networkResponse` 
+          const clonedResponse = networkResponse.clone();
+    
+          e.waitUntil((async function() {
+            const cache = await caches.open(CACHE_NAME);
+            // This will be called after `return networkResponse`
+            // so make sure you already have the clone!
+            await cache.put(e.request, clonedResponse);
+          })());
+        }
+    
+        return networkResponse;
+      })());
 });
